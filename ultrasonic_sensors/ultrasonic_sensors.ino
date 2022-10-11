893f033f306dc5c92f9131317ec50997118e68e9
saptangshuk <saptangshukaviraj@gmail.com>
2022-10-11 20:19:40 +0530

    Create ultrasonic_sensors.ino

---
+++ ultrasonic_sensors/ultrasonic_sensors.ino
@@ -0,0 +1,79 @@
+#define trigPin 2 
+#define echoPin 3
+#define trigPin1 4 
+#define echoPin1 5
+#define trigPin2 6 
+#define echoPin2 7
+#define trigPin3 8 
+#define echoPin3 9
+#define trigPin4 10 
+#define echoPin4 11
+long d1;
+long d2;
+long d3;
+long d4;
+long d5;
+
+void setup() 
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(trigPin3, OUTPUT);
+  pinMode(echoPin3, INPUT);
+  pinMode(trigPin4, OUTPUT);
+  pinMode(echoPin4, INPUT);
+  Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
+  Serial.println("Ultrasonic Sensor HC-SR04 Test"); // print some text in Serial Monitor
+  Serial.println("with Arduino UNO R3");
+}
+
+void loop() 
+{
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  d1 = pulseIn(echoPin, HIGH);
+
+  digitalWrite(trigPin1, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin1, LOW);
+  d2 = pulseIn(echoPin1, HIGH);
+
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  d3 = pulseIn(echoPin2, HIGH);
+
+  /*digitalWrite(trigPin3, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin3, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin3, LOW);
+  d4 = pulseIn(echoPin3, HIGH);*/
+
+
+  /*digitalWrite(trigPin4, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin4, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin4, LOW);
+  d5 = pulseIn(echoPin4, HIGH);*/
+
+  
+  Serial.println(d1);
+  Serial.println(d2);
+  Serial.println(d3);
+  Serial.println(d4);
+  Serial.println(d5);
+  Serial.println();
+}
