84f064c657bde1d531f2e4f1a1e2a5ec1795bc79
sandeep sahoo <sandeepsahoo542@gmail.com>
2022-10-26 22:43:52 +0530

    string update

--- ard_com_codes.py
+++ ard_com_codes.py
@@ -13,3 +13,14 @@ GPIO.setup(pin2,GPIO.IN)
 GPIO.setup(pin3,GPIO.IN)
 GPIO.setup(pin4,GPIO.IN)
 
+out1 = GPIO.input(pin1)
+out2 = GPIO.input(pin2)
+out3 = GPIO.input(pin3)
+out4 = GPIO.input(pin4)
+
+str = str(out1)+str(out2)+str(out3)+str(out4)
+
+if str == '0000':
+  #stable state
+else if str == '0001':
+  #continue making like this
